9828c3cb3be6111e7b06cb58ac9676215dfaeb5d
crisee <38986197+crisee@users.noreply.github.com>
2018-05-05 11:52:23 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,33 @@
+Config = {}
+Config.DrawDistance = 100.0
+
+Config.Zones = {
+
+	PoliceGet = {
+		Pos   = {x = 460.50, y = -988.90, z = 42.79},
+		Size  = {x = 3.5, y = 3.5, z = 0.6},
+		Color = {r = 255, g = 153, b = 0},
+		Type  = 27
+	},
+
+	PoliceRem = {
+		Pos   = {x = 481.68, y = -982.39, z = 40.10},
+		Size  = {x = 3.5, y = 3.5, z = 3.0},
+		Color = {r = 255, g = 0, b = 0},
+		Type  = 27
+	},
+
+	AmbulanceGet = {
+		Pos   = {x = 319.55, y = -1455.45, z = 45.60},
+		Size  = {x = 3.5, y = 3.5, z = 0.6},
+		Color = {r = 255, g = 153, b = 0},
+		Type  = 27
+	},
+
+	AmbulanceRem = {
+		Pos   = {x = 299.44, y = -1453.33, z = 45.60},
+		Size  = {x = 3.5, y = 3.5, z = 3.0},
+		Color = {r = 255, g = 0, b = 0},
+		Type  = 27
+	},
+}
